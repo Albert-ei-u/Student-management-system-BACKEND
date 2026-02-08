340131be01530128a0a76c0cde3a2cfad2bc7ea9
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-09 00:45:05 +0200

    Important updates were made

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const studentRoutes = require("./routes/studentRoutes");
 app.use("/students", studentRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
